69df78bdfd364e5f793e4bdebecd982a12bcc89f
Marko Kastratovic <marko.kastratovic@Soprex.local>
2022-08-21 13:42:59 +0200

    Added component for rendering single product and displaying all products from data.js file

---
+++ components/ProductItem.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import Link from 'next/link';
+
+export default function ProductItem(props) {
+  const product = props.product;
+
+  return (
+    <div className="card">
+      <Link href={`/products/${product.slug}`}>
+        <a>
+          <img
+            src={product.image}
+            alt={product.name}
+            className="rounded shadow"
+          />
+        </a>
+      </Link>
+
+      <div className="flex flex-col item-center justify-center p-5 items-center">
+        <Link href={`/products/${product.slug}`}>
+          <a>
+            <h2 className="text-lg">{product.name}</h2>
+          </a>
+        </Link>
+        <p className="mb-2">{product.brand}</p>
+        <p>${product.price}</p>
+        <button className="primery-button" type="button">
+          Add to cart
+        </button>
+      </div>
+    </div>
+  );
+}
